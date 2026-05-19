c1d1f15c189ec4202a4c79d900edac9887a0df7f
ifan0927 <ifan@estate.tw>
2026-05-19 14:46:01 +0800

    codecrafters submit [skip ci]

--- app/requestStruct.go
+++ app/requestStruct.go
@@ -20,31 +20,33 @@ func (r *RequestData) ParseRequest(request []byte, requestLength int) error {
 	buffer := request[:requestLength]
 	lines := strings.Split(string(buffer), CRLF)
 
-	parsingHeader := true
-	r.Headers = make(map[string]string)
-	for index, line := range lines {
-		if index == 0 { // request line
-			err := r.ParseLines(line)
-			if err != nil {
-				return err
-			}
-		} else if parsingHeader { //header
-			if line == "" {
-				parsingHeader = false
-			} else {
-				parts := strings.SplitN(line, ":", 2)
-				if len(parts) != 2 {
-					return fmt.Errorf("invalid header format at line %d", index)
-				}
-				for i, part := range parts {
-					parts[i] = strings.TrimSpace(part)
-				}
-				r.Headers[parts[0]] = parts[1]
-			}
-		} else { // body
-			r.Body += line + CRLF
-		}
+	// request line
+	err := r.ParseLines(lines[0])
+	if err != nil {
+		return err
 	}
+	// request header
+	var headerStop int
+	r.Headers = make(map[string]string)
+	for i := 1; i < len(lines); i++ {
+		if lines[i] == "" {
+			headerStop = i
+			break
+		}
+		parts := strings.SplitN(lines[i], ":", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("invalid header format at line %d", i)
+		}
+		for i, part := range parts {
+			parts[i] = strings.TrimSpace(part)
+		}
+		r.Headers[parts[0]] = parts[1]
+	}
+	// request body
+	for i := headerStop + 1; i < len(lines); i++ {
+		r.Body += lines[i] + CRLF
+	}
+
 	return nil
 }
 
