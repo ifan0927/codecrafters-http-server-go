de99bf240938506b7aca3d0019b9cf1f4bb3c5a4
ifan0927 <ifan@estate.tw>
2026-05-19 15:59:03 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
 
+var FILEPATH *string
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
+	FILEPATH = flag.String("directory", "/tmp/", "Path to the file to read")
+	flag.Parse()
 
 	for {
 		var conn net.Conn
@@ -92,6 +97,18 @@ func getResponseBasedOnFamilyPath(requestData RequestData) []byte {
 			}
 			length := len(pathSegments[i+1])
 			return []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", length, pathSegments[i+1]))
+		}
+		if strings.Contains(segment, "files") {
+			fmt.Println("File path: ", *FILEPATH)
+			if i == len(pathSegments)-1 {
+				return []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+			}
+			file, err := os.ReadFile(*FILEPATH + pathSegments[i+1])
+			if err != nil {
+				return nil
+			}
+			fmt.Println("File content: ", string(file))
+			return []byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(file), file))
 		}
 	}
 	return []byte("HTTP/1.1 404 Not Found\r\n\r\n")
